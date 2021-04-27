cccc4c8f15954049fc7ae2e6b960c90f0c9e96ce
James Bradbury <jamesbradbury93@gmail.com>
2021-04-27 23:56:38 +0100

    static adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,10 @@
+const adapt = require('@sveltejs/adapter-static');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		ssr: true
+		adapter: adapt()
 	}
 };
