e291519b19564dc185c0cef94cc0588afe91b0c8
James Bradbury <jamesbradbury93@gmail.com>
2021-05-04 18:54:30 +0100

    initiate sampler with 1.0 sample length

--- src/lib/rewire/sampler.js
+++ src/lib/rewire/sampler.js
@@ -4,7 +4,7 @@ import { Env } from '$lib/instruments/envelope.js';
 class Sampler {
     constructor(buffers) {
         this.players = [];
-        this.envelope = new Env(0.01, 10.0);
+        this.envelope = new Env(0.01, 1.0);
         this.out = new Tone.Gain(1.0).toDestination();
         this.envelope.out.connect(this.out);
         
@@ -18,6 +18,7 @@ class Sampler {
         }
     }
     trigger(time, sampleIdx, velocity, duration) {
+        console.log(duration)
         const play = this.players[sampleIdx];
         this.envelope.trigger(time, velocity)
         play.start(time, 0.0, duration)
