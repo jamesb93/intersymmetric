a347e8eba2e6d4beb1d38f47ee800cfbfeeeb1f3
James Bradbury <jamesbradbury93@gmail.com>
2021-05-05 21:36:10 +0100

    route for blasting the server

---
+++ src/routes/blaster.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { socket } from '$lib/components/stores.js';
+    let joined = false;
+    let grid = null;
+    let interval = null;
+    socket.emit('roomJoin', 'rewire.test');
+
+    socket.on('connect', () => {
+        joined = true;
+    })
+
+    socket.on('grid', x => {
+        grid = x
+        console.log(grid)
+    })
+
+    function go() {
+        if (interval === null) {
+            interval = setInterval(() => {
+                grid = grid.map(row => row.map(col => Math.random() < 0.5));
+                socket.emit('grid', grid);
+            }, 4)
+        } else {
+            clearInterval(interval)
+            interval = null;
+        }
+    }
+</script>
+<button on:click={go}> fuck the server up </button>
+<div id="container">
+{#if grid !== null}
+    {#each grid as row}
+    <br>
+    {#each row as col}
+        {col ? 1 : 0}
+    {/each}
+    {/each}
+{/if}
+</div>
+
+<style>
+    #container {
+        margin: 0 auto;
+        max-width: 50%;
+    }
+</style>
+
