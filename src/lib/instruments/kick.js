ff2440cc0422545b696777da748685274f7f2898
James Bradbury <jamesbradbury93@gmail.com>
2021-05-09 18:43:46 +0100

    oversample distortion

--- src/lib/instruments/kick.js
+++ src/lib/instruments/kick.js
@@ -5,6 +5,7 @@ class KickSynth {
         this.out = new Tone.Limiter(-1)
         this.distortion = new Tone.Distortion(0.0).connect(this.out);
         this.membrane = new Tone.MembraneSynth().connect(this.distortion);
+        this.distortion.oversample = "2x";
     }
     trigger(time, velocity, duration) {
         this.membrane.triggerAttackRelease(this.membrane.frequency.value, duration, time, velocity)
