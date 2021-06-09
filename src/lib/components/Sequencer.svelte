0af9f3a8cba5d6c76c3dc02d12faf71e258a0b86
James Bradbury <jamesbradbury93@gmail.com>
2021-06-09 20:45:23 +0100

    make scale better for sound

--- src/lib/components/Sequencer.svelte
+++ src/lib/components/Sequencer.svelte
@@ -158,8 +158,8 @@
             enabled={true}
             title="Sound"
             showTitle={false}
-            scale=0.001 min={0} max={1.0}
-            step={0.001}
+            scale=0.0025 min={0} max={1.0}
+            step={0.0001}
             bind:value={ $trackSound[x] } 
             func={ () => {updateSound(x); socket.emit('trackSound', $trackSound)} } 
             />
