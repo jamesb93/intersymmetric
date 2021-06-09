6b1e5f04a46b5dec24c1ff2a542ae9a03c3f85aa
James <jb@jamesbradbury.xyz>
2021-06-09 19:20:10 +0000

    update deployment addr

--- src/lib/components/stores.js
+++ src/lib/components/stores.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { writable } from 'svelte/store';
 
 // Sockets
-let socketAddr = import.meta.env.PROD ? "wss://8f43.xyz:4300" : "ws://localhost:4300"
+let socketAddr = import.meta.env.PROD ? "wss://interbackend.xyz:4300" : "ws://localhost:4300"
 
 export const socket = io(socketAddr);
 
