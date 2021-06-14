a15823b590ef615a7218e0c9aa9de5a15a79947b
James Bradbury <jamesbradbury93@gmail.com>
2021-06-14 23:00:06 +0100

    lower volume of metal.js

--- src/lib/instruments/metal.js
+++ src/lib/instruments/metal.js
@@ -2,7 +2,7 @@ import * as Tone from "tone";
 
 class MetalSynth {
     constructor() {
-        this.out = new Tone.Gain(0.3);
+        this.out = new Tone.Gain(0.21);
         this.limiter = new Tone.Limiter(-1.0).connect(this.out);
         this.comp = new Tone.Compressor(-12, 3).connect(this.limiter);
         this.source = new Tone.MetalSynth().connect(this.comp);
