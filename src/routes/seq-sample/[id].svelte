495ab10576077d48dc970e371aa7afa7e079d1c2
James Bradbury <jamesbradbury93@gmail.com>
2021-07-14 18:33:04 +0100

    page for sample player

---
+++ src/routes/seq-sample/[id].svelte
@@ -0,0 +1,40 @@
+<script context="module">
+	export async function load({ page }) {
+        return { 
+            props: {id: page.params.id}
+        };
+	}
+</script>
+
+<script>
+    import { browser } from '$app/env';
+	import { workshopID, room, socket } from '$lib/app.js';
+	import { createRoomID } from '$lib/utility.js';
+    export let id;
+
+	$workshopID = "nobounds.sample";
+    const roomCode = createRoomID($workshopID, id);
+
+    $room = id;
+    socket.emit('roomJoin', roomCode);
+</script>
+
+<div class="main-layout">
+	<Grid />
+	{#if browser}
+	<!-- Samplers -->
+	{/if}
+</div>
+	
+<style>
+	.main-layout {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.synth-controls {
+		display: flex;
+		flex-direction: row;
+	}
+</style>
---
+++ src/routes/seq-sample/index.svelte
@@ -0,0 +1,85 @@
+<script>
+    import { socket, room, workshopID } from "$lib/app.js"
+    import { goto } from '$app/navigation';
+    import { fade } from 'svelte/transition';
+    import { createRoomID } from '$lib/utility.js'
+	let everUsed = false;
+    let code = ""
+
+    let placeholder = '...'
+    let blink = null;
+    function createBlink() {
+        blink = setInterval(() => {
+            placeholder = placeholder === '...' ? '' : '...';
+        }, 750)
+    }
+
+    function stopBlink() {
+        clearInterval(blink);
+        placeholder = ''
+    }
+
+    createBlink();
+    
+    function handleChange() {
+        everUsed = true;
+        socket.emit('roomJoin', createRoomID($workshopID, code));
+        $room = code;
+        const href = '/seq-sample/' + code;
+        console.log(href);
+        goto('/seq-sample/'+ $room)
+        code = ""
+    }
+</script>
+
+<div class='container' transition:fade>
+    <span id='title'>Enter Room Code</span>
+    <input type='text'
+        class:init={!everUsed} 
+        bind:value={code} 
+        on:change={handleChange}
+        on:focus={ stopBlink }
+        on:blur={ createBlink }
+        placeholder={ everUsed === false && $room === '' ? placeholder : $room }
+        id='room-input'
+    />
+</div>
+
+
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        max-width: 100%;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        min-height: 100vh;
+    }
+
+    #room-input {
+        font-family: var(--font);
+        font-size: 20px;
+        padding: none;
+        background: white;
+        border: none;
+        width: 200px;
+        padding-top: 1px;
+        text-align: center;
+    }
+
+    #room-input:focus, #room-input:active {
+        outline: none !important;
+        border:1px solid rgb(204, 204, 204);
+    }
+    #title {
+        text-align: center;
+    }
+
+    .init {
+        border:1px solid rgb(204, 204, 204);
+    }
+
+
+</style>
