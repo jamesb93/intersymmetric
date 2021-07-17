39d6a1ebac2caa896966aa2f076b745bb1b1cb07
James Bradbury <jamesbradbury93@gmail.com>
2021-07-17 14:17:06 +0100

    move to b2

--- src/lib/samplerConfig.js
+++ src/lib/samplerConfig.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
-export const sampleResource = 'https://allthesounds.xyz/';
+export const sampleResource = 'https://f000.backblazeb2.com/file/intersymmetric/';
 export const info = writable('')
