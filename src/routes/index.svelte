781e6571c1ee785ec349ce55aa06259985715f5e
James Bradbury <jamesbradbury93@gmail.com>
2021-08-04 15:55:25 +0100

    add landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,46 @@
 <script>
+    import BoxButton from '$lib/components/BoxButton.svelte';
+    import { fade } from 'svelte/transition';
     import { goto } from '$app/navigation';
-    import { browser } from '$app/env';
 
-    if (browser)
-        goto('/seq-1');
+    function handleButton() {
+        // THIS LINK POINTS TO THE CURRENT THING
+        goto('/seq-1/idx')
+    }
+
 </script>
+
+<div class='container' transition:fade>
+    <span>Performances:</span>
+    <div class='times'>
+        <span>13/08 21:00 BST | Modern Institute</span>
+        <span>19/08 21:00 BST | Ewa Justke</span>
+        <span>26/08 21:00 BST | Beatrice Dillon</span>
+        <span>31/08 21:00 BST | Nyege Nyege</span>
+    </div>
+
+    <div class='info'> 
+        Instructions Go Here
+    </div>
+
+    <BoxButton text='Join' func={ handleButton } />
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        max-width: 100%;
+        justify-content: center;
+        align-items: center;
+        text-align: left;
+        min-height: 100vh;
+        gap: 20px;
+    }
+
+    .times{
+        display: flex;
+        flex-direction: column;
+    }
+
+</style>
