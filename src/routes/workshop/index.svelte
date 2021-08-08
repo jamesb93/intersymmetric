51a2b47c9e860b223842cbad6ad069fb645b1123
James Bradbury <jamesbradbury93@gmail.com>
2021-08-08 17:44:12 +0100

    send immediately to a room in workshop

--- src/routes/workshop/index.svelte
+++ src/routes/workshop/index.svelte
@@ -1,5 +1,10 @@
 <script>
-    import Landing from '$lib/components/Landing.svelte'
+    // import Landing from '$lib/components/Landing.svelte'
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+    onMount(async() => {
+        goto('workshop/1')
+    })
 </script>
 
-<Landing route='/seq-sample/' />
+<!-- <Landing route='/seq-sample/' /> -->
