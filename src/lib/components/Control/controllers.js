5bba5543e1be2056c882de7c1d570bdcea7e39fe
James Bradbury <me@jamesbradbury.net>
2022-01-19 18:49:32 +0000

    make controllers importable more cleanly

---
+++ src/lib/components/Control/controllers.js
@@ -0,0 +1,4 @@
+export { default as Kick } from './Kick.svelte';
+export { default as Snare} from './Snare.svelte';
+export { default as Metal } from './Metal.svelte';
+export { default as FM } from './FM.svelte';
