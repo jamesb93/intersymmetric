7bf3398ab7b538d32d220c62a2f8d783f7f511bb
James Bradbury <me@jamesbradbury.net>
2022-01-22 23:57:21 +0000

    new versions of poly-able instruments

---
+++ src/lib/components/test/instruments/envelope.js
@@ -0,0 +1,19 @@
+import * as Tone from 'tone';
+
+class Env {
+    constructor(attack, release) {
+        this.attack = attack;
+        this.release = release;
+        this.meter = new Tone.Meter({ normalRange: 1 });
+        this.out = new Tone.Multiply(0.0);
+        this.lp = new Tone.OnePoleFilter(50, 'lowpass').fan(this.out.factor, this.meter);
+        this.line = new Tone.Signal(0, { minValue: 0, maxValue: 1 }).connect(this.lp);
+    }
+
+    trigger(time, velocity) {
+        this.line.exponentialRampTo(1.0 * velocity, this.attack, time);
+        this.line.exponentialRampTo(0.0, this.release, time+this.attack);
+    }
+}
+
+export { Env }
