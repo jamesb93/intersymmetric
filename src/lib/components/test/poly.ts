6f4c25c0d15013ef1d287b0889d0d700b8878e17
James Bradbury <me@jamesbradbury.net>
2022-01-22 23:57:41 +0000

    update poly model

--- src/lib/components/test/poly.ts
+++ src/lib/components/test/poly.ts
@@ -3,15 +3,16 @@ class Poly {
     index: number;
 
     constructor(synth, numVoices) {
-        this.voices = new Array(numVoices).fill(new synth);
+        this.voices = new Array(numVoices).fill(null);
+        this.voices = this.voices.map(s => new synth);
         this.voices.forEach(s => s.out.toDestination())
         this.index = 0;
     }
 
-    trigger(time, velocity, duration) {
+    trigger(time, duration, parameters) {
+        this.voices[this.index].trigger(time, duration, parameters);
         this.index += 1;
         this.index = this.index % this.voices.length;
-        this.voices[this.index].trigger(time, velocity, duration);
     }
 }
 
