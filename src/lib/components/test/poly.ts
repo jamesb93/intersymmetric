95609a9bc8dd9a10c00683f3499658cf03b9cab2
James Bradbury <me@jamesbradbury.net>
2022-01-23 13:53:21 +0000

    use a queue to manage busy states

--- src/lib/components/test/poly.ts
+++ src/lib/components/test/poly.ts
@@ -1,18 +1,34 @@
+import { Queue } from './queue';
+
 class Poly {
     voices: any[];
     index: number;
+    queue: Queue;
 
     constructor(synth, numVoices) {
         this.voices = new Array(numVoices).fill(null);
         this.voices = this.voices.map(s => new synth);
         this.voices.forEach(s => s.out.toDestination())
-        this.index = 0;
+        this.queue = new Queue(numVoices);
     }
 
+    getFreeVoice() {
+        return (
+            this.voices.find(s => !s.busy)
+            ||
+            this.queue.peek()
+            || 
+            this.voices[0]
+        );
+    };
+
     trigger(time, duration, parameters) {
-        this.voices[this.index].trigger(time, duration, parameters);
-        this.index += 1;
-        this.index = this.index % this.voices.length;
+        const freeVoice = this.getFreeVoice();
+
+        if (freeVoice !== undefined && freeVoice !== null) {
+            this.queue.enqueue(freeVoice);
+            freeVoice.trigger(time, duration, parameters);
+        }
     }
 }
 
