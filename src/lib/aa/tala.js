1a7e4cafdcdcccf1a7e03cd637805e27d0e78030
James Bradbury <me@jamesbradbury.net>
2022-01-24 00:04:44 +0000

    add iterator for tala

--- src/lib/aa/tala.js
+++ src/lib/aa/tala.js
@@ -1,9 +1,21 @@
 class Tala {
     constructor(pattern) {
         this.pattern = pattern || [];
+        this.a = 0;
+        this.b = 0;
     }
 
-    
+    next() {
+        const ptn = [this.a, this.b];
+        this.b++;
+        this.b = this.b % this.pattern[this.a];
+
+        if (this.b === 0) {
+            this.a++;
+            this.a = this.a % this.pattern.length;
+        }
+        return ptn;
+    }
 }
 
 export { Tala }
