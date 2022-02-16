bb2c4e199e36615bff9444abc3df97e77248db4d
James Bradbury <me@jamesbradbury.net>
2022-02-16 23:14:12 +0000

    add send functions for socket.io

--- src/lib/aaa/interface/SoundPattern.svelte
+++ src/lib/aaa/interface/SoundPattern.svelte
@@ -1,5 +1,6 @@
 <script>
     import './interface.css';
+    import { socket } from '../app';
     import presets from '../../../../static/aaa/presets.json';
     import Knob from '../Knob.svelte';
     import Blip from '../Blip.svelte';
@@ -18,7 +19,8 @@
         max: presets.fm1_freq.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm1_freq_preset', $fm1_freq_preset) }
     }
     const fm1_mod_knob = {
         title: "mod",
@@ -26,7 +28,8 @@
         max: presets.fm1_mod.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm1_mod_preset', $fm1_mod_preset) }    
     }
     const fm1_shape_knob = {
         title: "shape",
@@ -34,7 +37,8 @@
         max: presets.fm1_shape.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm1_shape_preset', $fm1_shape_preset) }    
     }
     const fm2_freq_knob = {
         title: "freq",
@@ -42,7 +46,8 @@
         max: presets.fm2_freq.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm2_freq_preset', $fm2_freq_preset) }    
     }
     const fm2_mod_knob = {
         title: "mod",
@@ -50,7 +55,8 @@
         max: presets.fm2_mod.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm2_mod_preset', $fm2_mod_preset) }  
     }
     const fm2_shape_knob = {
         title: "shape",
@@ -58,31 +64,35 @@
         max: presets.fm2_shape.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('fm2_shape_preset', $fm2_shape_preset) }  
     }
     const perc_sound_knob = {
         title: "sound",
         min: 0,
-        max: presets.fm2_freq.length,
+        max: presets.perc_sound.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('perc_sound_preset', $perc_sound_preset) }  
     }
     const perc_transpose_knob = {
         title: "transpose",
         min: 0,
-        max: presets.fm2_mod.length,
+        max: presets.perc_sound.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('perc_transpose_preset', $perc_transpose_preset) }  
     }
     const perc_shape_knob = {
         title: "shape",
         min: 0,
-        max: presets.fm2_shape.length,
+        max: presets.perc_shape.length,
         step: 1,
         scale: 0.1,
-        resetValue: 0
+        resetValue: 0,
+        func: () => { socket.emit('perc_shape_preset', $perc_shape_preset) }
     }
 </script>
 
@@ -90,7 +100,7 @@
     <div class="label">sound pattern</div>
     <div class="controls">
         <div class='panel'>
-            <RadioV bind:value={$perc_listener} />
+            <RadioV func={ () => { socket.emit('perc_listener', $perc_listener) }} bind:value={$perc_listener} />
             <Blip />
             <Knob {...perc_sound_knob}  bind:value={$perc_sound_preset} />
             <Knob {...perc_transpose_knob}   bind:value={$perc_transpose_preset} />
@@ -98,7 +108,7 @@
         </div>
 
         <div class='panel'>
-            <RadioV bind:value={$fm1_listener} />
+            <RadioV func={ () => { socket.emit('fm1_listener', $fm1_listener) }} bind:value={$fm1_listener} />
             <Blip />
             <Knob {...fm1_freq_knob}  bind:value={$fm1_freq_preset} />
             <Knob {...fm1_mod_knob}   bind:value={$fm1_mod_preset} />
@@ -106,7 +116,7 @@
         </div>
 
         <div class='panel'>
-            <RadioV bind:value={$fm2_listener} />
+            <RadioV func={ () => { socket.emit('fm2_listener', $fm2_listener) }} bind:value={$fm2_listener} />
             <Blip />
             <Knob {...fm2_freq_knob}  bind:value={$fm2_freq_preset} />
             <Knob {...fm2_mod_knob}   bind:value={$fm2_mod_preset} />
