b6554401a25d1062e22de4a216965402105fcbec
James Bradbury <github@jamesbradbury.net>
2022-07-21 21:09:22 +0100

    template out front page

---
+++ src/routes/nyege/__layout.svelte
@@ -0,0 +1,25 @@
+
+<script>
+    import '../../sf.css'
+    import Header from '$lib/aaa/header/Header.svelte';
+    import Footer from '$lib/aaa/Footer.svelte';
+</script>
+
+<div class="container">
+    <Header />
+    <slot />
+    <Footer />
+</div>
+
+<style>
+    .container {
+        max-width: 1005px;
+        margin: 0 auto;
+        user-drag: none;
+        -webkit-user-drag: none;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
+</style>
