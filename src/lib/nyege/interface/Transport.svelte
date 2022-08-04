eaedefc55140d61a4ae60fa61cb8e751bb64f468
James Bradbury <github@jamesbradbury.net>
2022-08-04 14:09:03 +0100

    start adding messages and interface to transport

--- src/lib/nyege/interface/Transport.svelte
+++ src/lib/nyege/interface/Transport.svelte
@@ -1,7 +1,15 @@
 <script>
-	import Play from '$lib/nyege/Play.svelte'
+	import Play from '$lib/nyege/Play.svelte';
+	import Knob from '$lib/nyege/Knob.svelte';
+	import { send_message } from '$lib/aaa/patch_helpers';
 
 	export let patch;
+
+	let state = 0;
+
+	$: send_message(patch, 'state', [state]);
 </script>
 
-<Play />
+<div class="container">
+	<Play bind:state />
+</div>
