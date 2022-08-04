b0ef608d9655243aff1f688710c6278a749b33d0
James Bradbury <github@jamesbradbury.net>
2022-08-04 14:09:14 +0100

    implement backend data

--- src/lib/nyege/app.js
+++ src/lib/nyege/app.js
@@ -26,13 +26,13 @@ export const d_sound = writable(0);
 export const e_sound = writable(0);
 export const f_sound = writable(0);
 export const g_sound = writable(0);
-socket.on('a_sound', x => a_sound.set(x));
-socket.on('b_sound', x => b_sound.set(x));
-socket.on('c_sound', x => c_sound.set(x));
-socket.on('d_sound', x => d_sound.set(x));
-socket.on('e_sound', x => e_sound.set(x));
-socket.on('f_sound', x => f_sound.set(x));
-socket.on('g_sound', x => g_sound.set(x));
+socket.on('a_sound', (x) => a_sound.set(x));
+socket.on('b_sound', (x) => b_sound.set(x));
+socket.on('c_sound', (x) => c_sound.set(x));
+socket.on('d_sound', (x) => d_sound.set(x));
+socket.on('e_sound', (x) => e_sound.set(x));
+socket.on('f_sound', (x) => f_sound.set(x));
+socket.on('g_sound', (x) => g_sound.set(x));
 export const a_pitch = writable(0);
 export const b_pitch = writable(0);
 export const c_pitch = writable(0);
@@ -40,13 +40,13 @@ export const d_pitch = writable(0);
 export const e_pitch = writable(0);
 export const f_pitch = writable(0);
 export const g_pitch = writable(0);
-socket.on('a_pitch', x => a_pitch.set(x));
-socket.on('b_pitch', x => b_pitch.set(x));
-socket.on('c_pitch', x => c_pitch.set(x));
-socket.on('d_pitch', x => d_pitch.set(x));
-socket.on('e_pitch', x => e_pitch.set(x));
-socket.on('f_pitch', x => f_pitch.set(x));
-socket.on('g_pitch', x => g_pitch.set(x));
+socket.on('a_pitch', (x) => a_pitch.set(x));
+socket.on('b_pitch', (x) => b_pitch.set(x));
+socket.on('c_pitch', (x) => c_pitch.set(x));
+socket.on('d_pitch', (x) => d_pitch.set(x));
+socket.on('e_pitch', (x) => e_pitch.set(x));
+socket.on('f_pitch', (x) => f_pitch.set(x));
+socket.on('g_pitch', (x) => g_pitch.set(x));
 export const a_length = writable(250);
 export const b_length = writable(250);
 export const c_length = writable(250);
@@ -54,31 +54,31 @@ export const d_length = writable(250);
 export const e_length = writable(250);
 export const f_length = writable(250);
 export const g_length = writable(250);
-socket.on('a_length', x => a_length.set(x));
-socket.on('b_length', x => b_length.set(x));
-socket.on('c_length', x => c_length.set(x));
-socket.on('d_length', x => d_length.set(x));
-socket.on('e_length', x => e_length.set(x));
-socket.on('f_length', x => f_length.set(x));
-socket.on('g_length', x => g_length.set(x));
+socket.on('a_length', (x) => a_length.set(x));
+socket.on('b_length', (x) => b_length.set(x));
+socket.on('c_length', (x) => c_length.set(x));
+socket.on('d_length', (x) => d_length.set(x));
+socket.on('e_length', (x) => e_length.set(x));
+socket.on('f_length', (x) => f_length.set(x));
+socket.on('g_length', (x) => g_length.set(x));
 
 // Polymetric
 export const a_subdiv = writable(16);
 export const b_subdiv = writable(16);
 export const c_subdiv = writable(16);
 export const d_subdiv = writable(16);
-socket.on('a_subdiv', x => a_subdiv.set(x));
-socket.on('b_subdiv', x => b_subdiv.set(x));
-socket.on('c_subdiv', x => c_subdiv.set(x));
-socket.on('d_subdiv', x => d_subdiv.set(x));
+socket.on('a_subdiv', (x) => a_subdiv.set(x));
+socket.on('b_subdiv', (x) => b_subdiv.set(x));
+socket.on('c_subdiv', (x) => c_subdiv.set(x));
+socket.on('d_subdiv', (x) => d_subdiv.set(x));
 export const a_range = writable(16);
 export const b_range = writable(16);
 export const c_range = writable(16);
 export const d_range = writable(16);
-socket.on('a_range', x => a_range.set(x));
-socket.on('b_range', x => b_range.set(x));
-socket.on('c_range', x => c_range.set(x));
-socket.on('d_range', x => d_range.set(x));
+socket.on('a_range', (x) => a_range.set(x));
+socket.on('b_range', (x) => b_range.set(x));
+socket.on('c_range', (x) => c_range.set(x));
+socket.on('d_range', (x) => d_range.set(x));
 
 // Retriggers
 
@@ -92,3 +92,17 @@ export const palette = {
 
 export const primary = palette.gold;
 export const primary_darker = palette.blue;
+
+export const subdiv = {
+	a: writable(16),
+	b: writable(16),
+	c: writable(16),
+	d: writable(16)
+};
+
+export const range = {
+	a: writable(16),
+	b: writable(16),
+	c: writable(16),
+	d: writable(16)
+};
