cdc551ee09527ab22f161dc2345812fdbbee5259
James Bradbury <github@jamesbradbury.net>
2022-08-09 00:04:11 +0100

    move patch move patch helpers to common

--- src/lib/aaa/patch_helpers.js
+++
@@ -1,9 +0,0 @@
-import RNBO from '@rnbo/js';
-
-const send_message = (patch, key, data) => {
-	if (patch && RNBO) {
-		patch.scheduleEvent(new RNBO.MessageEvent(RNBO.TimeNow, key, data));
-	}
-};
-
-export { send_message };
