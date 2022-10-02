ac6855bfe9aceacc85c321c5d204b66218f72671
James Bradbury <github@jamesbradbury.net>
2022-10-02 12:32:46 +0200

    only show snapshots if they exist

--- src/lib/nyege/interface/Snapshots.svelte
+++ src/lib/nyege/interface/Snapshots.svelte
@@ -73,9 +73,11 @@
 
 {#if !hide}
 <div class='container'>
+	{#if snapshots.length > 0}
 	{#each snapshots.slice(0, 128) as snap}
 	<button class='preset' on:click={() => recall(snap.state)}></button>
 	{/each}
+	{/if}
 </div>
 {/if}
 
