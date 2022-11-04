c7ca2b4a753be9baaf79a7c1bb8cc9ff2a082a4a
James Bradbury <github@jamesbradbury.net>
2022-11-04 23:16:31 +0100

    remove old css

--- src/lib/nyege/Interface.svelte
+++ src/lib/nyege/Interface.svelte
@@ -25,40 +25,4 @@
         margin: 0 auto;
         margin-top: 2em;
     }
-    .tab {
-        position: fixed;
-        top: 75px;
-        display: grid;
-        gap: 10px;
-        grid-template-rows: auto auto;
-        right: 0px;
-    }
-    button {
-        background-color: white;
-        border: 1px solid var(--primary);
-        font-family: var(--font);
-        font-size: 10px;
-        color: var(--primary);
-        width: 30px;
-        height: 100px;
-        text-align: center;
-    }
-    button:hover:not(.selected) {
-        background-color: var(--light-grey);
-    }
-    button:active:not(.selected) {
-        background-color: var(--grey);
-    }
-    .disabled {
-        opacity: 0.5;
-    }
-    .bottom {
-        display: flex;
-        flex-direction: column;
-        gap: 2em;
-    }
-    .hide {
-        visibility: hidden;
-        height: 0;
-    }
 </style>
