b85a5aa26e1edf7d8b35b8b8d8e7f7a2f7a7ae0d
James Bradbury <github@jamesbradbury.net>
2022-11-05 18:53:54 +0100

    create iphone side profile svg

---
+++ src/lib/nyege/interface/IPhoneSideProfile.svelte
@@ -0,0 +1,25 @@
+<svg width="366" height="49" xmlns="http://www.w3.org/2000/svg">
+	
+	<rect rx="8" id="svg_1" height="27" width="344.99999" y="10" x="11" stroke="#0540f6" fill="#ffffff"/>
+	<rect id="svg_7" height="5" width="28" y="20" x="81.5" stroke="#0540f6" fill="#ce5738"/>
+	<line id="svg_8" y2="38" x2="59.5" y1="11" x1="59.5" stroke="#0540f6" fill="none"/>
+	<rect id="svg_10" height="1" width="0" y="62" x="120.5" stroke="#0540f6" fill="none"/>
+	<rect class:toggle id="svg_12" height="5" width="28" y="22" x="81.5" stroke="#0540f6" fill="#f2f2f2"/>
+	<rect id="svg_13" height="7" width="36" y="20" x="164.5" stroke="#0540f6" fill="#f2f2f2"/>
+	<rect id="svg_17" height="7" width="36" y="20" x="126.5" stroke="#0540f6" fill="#f2f2f2"/>
+</svg>
+
+<style>
+	
+	.toggle {
+		transform: translate(0, -2px);
+	}
+</style>
+
+<script>
+	let toggle = false;
+	
+	setInterval(() => {
+		toggle = !toggle;
+	}, 1000)
+</script>
