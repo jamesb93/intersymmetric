30ee1d2549acbc6c9db4e923db0b58f379c2fc2a
James Bradbury <github@jamesbradbury.net>
2022-11-23 00:34:41 +0100

    add firebase to core

---
+++ src/lib/core.js
@@ -0,0 +1,12 @@
+import { ref, onValue } from 'firebase/database';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQ-SUfm2OZ_i4UCSK4qGfqZeflaewS004",
+    authDomain: "intersymmetric-7e851.firebaseapp.com",
+    databaseURL: "https://intersymmetric-7e851-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "intersymmetric-7e851",
+    storageBucket: "intersymmetric-7e851.appspot.com",
+    messagingSenderId: "500933689703",
+    appId: "1:500933689703:web:dd664fc065f7b9cce22fc2",
+    measurementId: "G-Q9HCJJXHGM"
+};
