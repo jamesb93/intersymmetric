7bc08db6a10a394884419e02633684b9c467c407
James Bradbury <github@jamesbradbury.net>
2022-11-23 00:35:13 +0100

    remove socket-based room joining

--- src/routes/nnnb/[id]/+page.svelte
+++ src/routes/nnnb/[id]/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-    import { room, socket } from '$lib/nyege/app';
-    import { createRoomID } from '$lib/utility';
+    import { room } from '$lib/nyege/app';
     import Instance from '$lib/nyege/Instance.svelte';
 
     /** @type {import('./$types').PageData} */
     export let data;
 
     $room = data.id;
-    socket.emit('join_room', createRoomID('nnnb', data.id));
 </script>
 
 <!-- stuff -->
