b1432f435e48b6a4a96906234f25c48b73b7bf3c
James Bradbury <github@jamesbradbury.net>
2022-11-23 23:22:50 +0100

    remove old config

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
         adapter: adapter(),
-        trailingSlash: 'always'
     },
 
     preprocess: [
