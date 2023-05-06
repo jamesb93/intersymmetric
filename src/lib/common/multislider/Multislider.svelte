32b2a37593710f6c646bff9a23794a91650b3f13
James Bradbury <github@jamesbradbury.net>
2023-05-06 23:33:39 +0200

    rainbow multislider

--- src/lib/common/multislider/Multislider.svelte
+++ src/lib/common/multislider/Multislider.svelte
@@ -1,52 +1,137 @@
 <script>
-	import Slider from './Slider.svelte';
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
 	import { CircularBuffer } from '$lib/common/queue';
+	import { CanvasSpace, Create, Group, Rectangle } from 'pts';
+	import { linearInterp, scale, clip } from '$lib/utility';
 	export let data = [];
 	export let width = 100
 	export let height = 100
+	export let colour = 'black'
+	export let maxWidth = 100
+	export let maxHeight = 100
+	export let margin = 0
+	export let scheme = null;
 
-	let svg;
+		// Define the color scheme
+	// const colorScheme = d3.schemeCategory10;
+	// const colorScheme = d3.interpolateSinebow()
+
+	// Define the interpolator function
+	// const interpolateColors = d3.interpolate(...colorScheme);
+
+	
 	let listening = false;
-	let buf = new CircularBuffer(2)
-
-    function moveHandler(e) {
-		buf.enqueue(e);
-    }
-
-    function downHandler(e) {
-        listening = true;
-    }
-
-    function upHandler(e) {
-        listening = false;
-    }
-
-    // Once mouse is down we start to listen for points that have been passed through
-    // We're essentially collecting tuples of points (1, 2, 10), (2, 10)
-    // Once listening has begun, each tick of the mousemove handler looks at these tuples sand interpolates and updates the sliders accordingly
+	let canvas;
+	let rect;
+	let space;
+	let form;
+	let buf = new CircularBuffer(2);
+	
+	function getMousePos(canvas, evt) {
+		// We need to do this manually otherwise when shifting the window the resize is not accounted for.
+		rect = canvas.getBoundingClientRect();
+		return {
+			x: (evt.clientX - rect.left),
+			y: (evt.clientY - rect.top)
+		}
+	}
+	
+	onMount(async() => {
+		rect = canvas.getBoundingClientRect();
+		space = new CanvasSpace('#sketch')
+		.setup({
+			bgcolor: '#fff',
+			resize: true,
+		});
+		form = space.getForm();
+		
+		space.add({
+			animate: (time, ftime, space) => {
+				const barWidth = space.width / data.length;
+				data.forEach((x, i) => {
+					const left = i * barWidth;
+					const r = Rectangle.fromTopLeft(
+						[left, space.height], 
+						barWidth,  
+						1-x * space.height
+					);
+					// const colour = interpolateColors(i / data.length);
+					const colour = d3.interpolateSinebow(i / data.length)
+					form.fill(colour, 1).rect( r );
+				})
+			},
+			action: (t, x, y, event) => {
+				if (t === 'move' && listening) {
+					buf.enqueue(event);
+					
+					const canvasWidth = rect.right - rect.left;
+					if (buf.isFull()) {
+						// a is now, b is history
+						const a = getMousePos(canvas, buf.get(0));
+						const b = getMousePos(canvas, buf.get(1));
+						a.idx = clip(
+							Math.floor((a.x / canvasWidth) * data.length),
+							0, data.length-1
+						)
+						
+						b.idx = clip(
+							Math.floor((b.x / canvasWidth) * data.length),
+							0, data.length-1
+						)
+						const numIntersects = Math.abs(a.idx - b.idx);
+						
+						if (numIntersects > 0) {
+							const start = b.y / (rect.bottom - rect.top);
+							const end = a.y / (rect.bottom - rect.top);
+							const isRev = b.idx > a.idx;
+							
+							for (let i=0; i<numIntersects; i++) {
+								
+								const interp = i / numIntersects;
+								
+								let value = 0.0;
+								if (isRev) {
+									value = linearInterp(end, start, interp);
+								} else{
+									value = linearInterp(start, end, interp);
+								}
+								const clampedIndex = clip(
+									Math.min(a.idx, b.idx)+i,
+									0, data.length-1
+								)
+								data[clampedIndex] = 1-value;
+								
+							}
+						} else {
+							data[b.idx] = 1- (b.y / (rect.bottom - rect.top));
+						}
+					}
+				}
+			}			
+		});
+		space.play().bindMouse();
+	})
 </script>
 
-<svelte:window on:mousemove={moveHandler} on:mouseup={upHandler} />
-
-<svg 
-width={width} 
-height={height} 
-on:mousedown={downHandler}
-bind:this={svg}
->
-	{#each data as d, i}
-		<Slider
-		bind:data={data}
-		value={d}
-		index={i}
-		parentHeight={height}
-		parentWidth={width}
-		/>
-	{/each}
-</svg>
-
+<svelte:window on:mouseup={() => { 
+	listening = false 
+	buf.clear();
+}} />
+<canvas 
+id='sketch' 
+bind:this={canvas} 
+on:mousedown={() => listening = true} 
+style:border-color={colour}
+style:width={width}
+style:max-width={maxWidth}
+style:max-height={maxHeight}
+style:height={height}
+/>
+	
 <style>
-	svg {
+	#sketch {
 		border: 1px solid black;
+		border-color: black;
 	}
 </style>
--- src/routes/widgets/+page.svelte
+++ src/routes/widgets/+page.svelte
@@ -1,109 +1,16 @@
 <script>
-	import Multislider from "$lib/common/multislider/Multislider.svelte"
-	let height = 100
-	let width = 400
-	let data = Array.from(new Array(128), x => Math.random())
+	import MultiSlider from '$lib/common/multislider/MultiSlider.svelte';
+	const length = 200;
+	let data = Array.from({ length }, () => Math.random());
 
-	let listening = false;
-	let buf = new CircularBuffer(2);
-	import { CanvasSpace, Create, Group, Rectangle } from 'pts';
-	import { onMount } from 'svelte';
-	import { clip } from '$lib/utility';
-    import { CircularBuffer } from '../../lib/common/queue';
-	import { linearInterp, scale } from '$lib/utility';
-	let canvas;
-	let rect;
-	let space;
-	let form;
-
-    function getMousePos(canvas, evt) {
-        // We need to do this manually otherwise when shifting the window the resize is not accounted for.
-        rect = canvas.getBoundingClientRect();
-        return {
-			x: (evt.clientX - rect.left),
-            y: (evt.clientY - rect.top)
-		}
-    }
-
-	onMount(async() => {
-		rect = canvas.getBoundingClientRect();
-		space = new CanvasSpace('#sketch')
-			.setup({
-				bgcolor: '#fff',
-				resize: true,
-			});
-		form = space.getForm();
-
-		space.add({
-			animate: (time, ftime, space) => {
-				const barWidth = space.width / data.length;
-				data.forEach((x, i) => {
-					const left = i * barWidth;
-					const r = Rectangle.fromTopLeft([left, x * space.height], barWidth,  space.height);
-					form.fill("#FF5733", 3 ).rect( r );
-				})
-			},
-			action: (t, x, y, event) => {
-				if (t === 'move' && listening) {
-					buf.enqueue(event);
-					
-					const canvasWidth = rect.right - rect.left;
-					if (buf.isFull()) {
-						// a is now, b is history
-						const a = getMousePos(canvas, buf.get(0));
-						const b = getMousePos(canvas, buf.get(1));
-						a.idx = clip(
-							Math.floor((a.x / canvasWidth) * data.length),
-							0, data.length
-						)
-
-						b.idx = clip(
-							Math.floor((b.x / canvasWidth) * data.length),
-							0, data.length
-						)
-						const numIntersects = Math.abs(a.idx - b.idx);
-						
-						if (numIntersects > 0) {
-							const start = b.y / (rect.bottom - rect.top);
-							const end = a.y / (rect.bottom - rect.top);
-							const isRev = b.idx > a.idx;
-					
-							for (let i=0; i < numIntersects; i++) {
-								
-								const interp = i / numIntersects;
-								
-								let value = 0.0;
-								if (isRev) {
-									value = linearInterp(end, start, interp);
-								} else{
-									value = linearInterp(start, end, interp);
-								}
-								data[Math.min(a.idx, b.idx)+i] = value;
-								
-							}
-						} else {
-							data[b.idx] = b.y / (rect.bottom - rect.top);
-						}
-					}
-				}
-			}			
-		});
-        space.play().bindMouse();
-	})
 </script>
 
-<svelte:window on:mouseup={() => { 
-	listening = false 
-	buf.clear();
-}} />
-<canvas id='sketch' bind:this={canvas} on:mousedown={() => listening = true} />
-
-<style>
-	#sketch {
-        width: 90%;
-		max-width: 50%;
-        height: 200px;
-		max-height: 200px;
-		border: 2px solid black;
-    }
-</style>
+<MultiSlider 
+bind:data={data}
+	width='400px'
+	maxWidth='50%'
+	height='150px'
+	maxHeight='150px'
+	colour='darkblue'
+	margin=100
+/>
