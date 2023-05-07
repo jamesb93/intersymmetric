ad8f94f3714ba9f8760e650137ce2282e9c96c31
James Bradbury <github@jamesbradbury.net>
2023-05-07 15:18:10 +0200

    working framework for RSS

---
+++ src/lib/rss/Instance.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { createDeviceInstance } from '@jamesb93/rnbo-svelte';
+    import Interface from './Interface.svelte';
+
+    /** @type {import('@rnbo/js').Device} */
+    let patch;
+
+    const start = async () => {
+        const context = new (window.AudioContext || window.webkitAudioContext)();
+        const output = context.createGain().connect(context.destination);
+        // @ts-ignore
+        createDeviceInstance('/rss/code/patch.export.json', context, output)
+        .then(response => patch = response);
+
+    };
+</script>
+
+{#if patch}
+    <Interface bind:patch />
+{:else}
+    <button class='start-btn' on:click={start}>
+        start
+    </button>
+{/if}
+
+<style>
+    .start-btn {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/lib/utility.js
+++ src/lib/utility.js
@@ -99,20 +99,16 @@ const fmod = (a, b) => {
 export const wrap = (i, min, max) => {
     let retVal;
     let range = max - min;
-
     if (i < max && i >= min) {
         return i;
     }
-
     if (max == min) {
         return min;
     }
-
     if (i < min) {
         retVal = i;
         while (retVal < min) retVal += range;
     } else retVal = fmod(i - min, range) + min;
-
     return retVal;
 };
 
