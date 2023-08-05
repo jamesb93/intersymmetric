e762e8256f73c1169da96c688766b0a26e1be3bf
James Bradbury <github@jamesbradbury.net>
2023-08-05 18:34:41 +0200

    create a vertical and horizontal meter

---
+++ src/lib/seq-2/HMeter.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	export let width = 128;
+	export let height = 16;
+	export let level = 0.0;
+</script>
+
+<div style:width={`${width}px`} style:height={`${height}px`}>
+	<svg height=100% width=100%>
+		<rect x=0 y={0} width={width * level} height={height} />
+	</svg>
+</div>
+
+<style>
+	div {
+		border: 1px solid var(--primary);
+	}
+
+	rect {
+		fill: var(--primary);
+	}
+</style>
+
