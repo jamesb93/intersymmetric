33214939eae86f0cfebd684d0b9594b4ace7213e
James Bradbury <github@jamesbradbury.net>
2023-08-05 18:36:47 +0200

    add placeholder component for instrument control

---
+++ src/lib/seq-2/InstrumentControl.svelte
@@ -0,0 +1,41 @@
+<script lang='ts'>
+    import HMeter from './HMeter.svelte';
+	import Knob from './Knob.svelte';
+	// export let instrument;
+	// const { sound, pitch, shape, level } = instrument;
+
+	// const sound_knob = {
+	// 	min: sound.get().min,
+	// 	max: sound.get().max,
+	// 	step: sound.get().step,
+	// 	resetValue: sound.init(),
+	// 	scale: 1,
+	// 	title: 'Sound'
+	// }
+
+	// const pitch_knob = {
+	// 	min: pitch.get().min,
+	// 	max: pitch.get().max,
+	// 	step: pitch.get().step,
+	// 	resetValue: pitch.init(),
+	// 	scale: 1,
+	// 	title: 'Pitch'
+	// }
+
+	// const shape_knob = {
+	// 	min: shape.get().min,
+	// 	max: shape.get().max,
+	// 	step: shape.get().step,
+	// 	resetValue: shape.init(),
+	// 	scale: 1,
+	// 	title: 'Shape'
+	// }
+
+</script>
+
+<div>
+	<!-- <Knob {...sound_knob} bind:value={$sound}/>
+	<Knob {...pitch_knob} bind:value={$pitch}/>
+	<Knob {...shape_knob} bind:value={$shape}/>
+	<HMeter bind:level={$level} /> -->
+</div>
