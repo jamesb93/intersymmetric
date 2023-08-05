a6e0a4a754e2aabbdeed97b9a9089ea959054286
James Bradbury <github@jamesbradbury.net>
2023-08-05 18:35:55 +0200

    add grip

--- src/lib/seq-2/Grid.svelte
+++ src/lib/seq-2/Grid.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
 	import type RNBO from '@rnbo/js'
-	import { grid } from '$lib/seq-2/app'
+	import { grid, grid_step } from '$lib/seq-2/app'
 	import { sendDeviceMessage } from '$lib/common/rnbo/helpers';
 
 	export let device: RNBO.Device;
@@ -10,13 +10,19 @@
 		const flat = $grid.flat();
 		sendDeviceMessage(device, 'grid', flat);
 	}
+
 </script>
 
 <div class="container">
 	{#each $grid as row, i}
 	<div class="row">
 		{#each row as cell, j}
-		<div class='col' class:enabled={cell}  on:click={() => handle_click(i, j)}/>
+		<div class='cell' 
+		class:step-active={$grid_step === j}  
+		class:enabled={cell}
+		on:click={() => handle_click(i, j)}
+		on:keydown={() => handle_click(i, j)}
+		/>
 		{/each}
 	</div>
 	{/each}
@@ -28,22 +34,33 @@
 		flex-direction: column;
 		gap: 0.25em;
 	}
-	.col {
-		width: 50px;
-		min-width: 50px;
-		max-width: 50px;
-		height: 50px;
-		border: 1px solid var(--primary)
-	}
-	.enabled {
-		background-color: var(--primary)
-	}
-
 	.row {
 		display: flex;
 		flex-direction: row;
 		gap: 0.25em;
 	}
+
+	@media only screen and (max-width: 768px) {
+		.container { flex-direction: row }
+		.row { flex-direction: column }
+	}
+
+	.step-active {
+		background-color: rgba(76, 40, 129, 0.232) !important;
+   	}
+	.cell {
+		width: 25px;
+		height: 25px;
+		min-width: 25px;
+		max-width: 25px;
+		border: 1px solid var(--primary);
+	}
+
+	.enabled {
+		background-color: var(--primary)
+	}
+
+
 
 
 </style>
